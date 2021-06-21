934b33a9d2bbc075ab948105d409faf1f248d090
mits003 <miyake@mierune.co.jp>
2021-06-21 10:07:44 +0900

    format

--- elevation_tile_tools/__main__.py
+++ elevation_tile_tools/__main__.py
@@ -31,14 +31,15 @@ from elevation_tile_tools.elevation_tile_converter import \
     default=10,
     help="Tile zoomlevel to get. default=10",
 )
-@click.option("--bbox",
-              "-b",
-              required=False,
-              type=str,
-              default=[141.24847412109375, 42.974511174899156, 141.48193359375, 43.11702412135048],
-              help="Extent of DEM tile to get. Specify with bounding box. default="
-                   "[141.24847412109375,42.974511174899156,141.48193359375,43.11702412135048]",
-              )
+@click.option(
+    "--bbox",
+    "-b",
+    required=False,
+    type=str,
+    default=[141.24847412109375, 42.974511174899156, 141.48193359375, 43.11702412135048],
+    help="Extent of DEM tile to get. Specify with bounding box. default="
+        "[141.24847412109375,42.974511174899156,141.48193359375,43.11702412135048]",
+    )
 def main(output_path, crs_id, zoom_level, bbox):
     if isinstance(bbox, str):
         bbox = eval(bbox)
