e774099b02681539b94f96e34f9c315de13960ff
mits003 <miyake@mierune.co.jp>
2021-06-22 16:55:40 +0900

    ファイルの冒頭にライセンス情報を追加

--- get_tiles.py
+++ get_tiles.py
@@ -1,3 +1,24 @@
+"""
+/***************************************************************************
+ ElevationTile4JP
+                                 A QGIS plugin
+ 画面内の標高タイルを取得し、GeoTiffに変換します
+                             -------------------
+        begin                : 2021-05-31
+        git sha              : $Format:%H$
+        copyright            : (C) 2021 by MIERUNE Inc.
+        email                : info@mierune.co.jp
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 import os
 from math import log
 
