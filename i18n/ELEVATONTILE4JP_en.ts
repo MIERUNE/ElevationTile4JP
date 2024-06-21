c511c8d3d93d6342f7f516136d89f1e2c87d438d
bordoray <lay.ishizuka@gmail.com>
2024-06-21 09:19:40 +0900

    English ts first round

--- i18n/ELEVATONTILE4JP_en.ts
+++ i18n/ELEVATONTILE4JP_en.ts
@@ -4,7 +4,7 @@
     <name>Dialog</name>
     <message encoding="UTF-8">
         <location filename="../progress_dialog.ui" line="20"/>
-        <source>処理中...</source>
+        <source>Processing...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -14,12 +14,12 @@
     </message>
     <message encoding="UTF-8">
         <location filename="../progress_dialog.ui" line="51"/>
-        <source>テキスト</source>
+        <source>Something</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
         <location filename="../progress_dialog.ui" line="69"/>
-        <source>中断</source>
+        <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -32,17 +32,17 @@
     </message>
     <message encoding="UTF-8">
         <location filename="../elevation_tile_for_jp_dialog_base.ui" line="42"/>
-        <source>標高タイルズームレベル</source>
+        <source>Elevation tile zoom level</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
         <location filename="../elevation_tile_for_jp_dialog_base.ui" line="73"/>
-        <source>出力ファイル</source>
+        <source>Output file path</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
         <location filename="../elevation_tile_for_jp_dialog_base.ui" line="113"/>
-        <source>出力ファイルCRS</source>
+        <source>Output file CRS</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
